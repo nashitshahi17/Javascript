28965b397e5f73f8058a3b585938f99aa8ab5961
nashithshahi17 <nashitshahi17@gmail.com>
2026-05-21 07:23:52 +0530

    initial commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treal all JS code as newer version 
+
+let name = "Babar"
+let age = 34
+let isLoggedIN = true
+
+// number => 2 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null) // the type of null is object
