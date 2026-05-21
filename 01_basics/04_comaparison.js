059abe14a9ea416a66d9a6cf963d48eceef3f13c
nashithshahi17 <nashitshahi17@gmail.com>
2026-05-21 18:19:34 +0530

    initial commit

---
+++ 01_basics/04_comaparison.js
@@ -0,0 +1,7 @@
+console.log("2">1) // true 
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null>=0) //true
+
+console.log("2" == 2) // true because it converts to same datatype
+console.log("2"===2) // false because it checks strictly with the datatype
